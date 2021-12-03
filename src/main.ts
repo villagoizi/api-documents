374c410ee491d84d0176b1b6eacd441f8d2dfc6d
beetlejuice96 <dilanbernbae043@gmail.com>
2021-12-02 23:40:31 -0300

    change main & change upload controller

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        host: process.env.APP_HOST,
-        port: +process.env.APP_PORT,
-      },
-    },
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000, () =>
+    console.log(
+      `Microservice api-gateway running http://${process.env.APP_HOST}:${process.env.APP_PORT}`,
+    ),
   );
-  app
-    .listen()
-    .then(() =>
-      console.log(
-        `Microservice api-document running in http://${process.env.APP_HOST}:${process.env.APP_PORT}`,
-      ),
-    );
 }
 bootstrap();
